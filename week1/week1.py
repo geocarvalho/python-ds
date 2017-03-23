bc67afaf0c642c7def937d981032a24d10076ce4
george <geocarvalho@outlook.com>
2017-03-23 00:53:26 -0300

    week one done

--- week1/week1.py
+++ week1/week1.py
@@ -54,3 +54,97 @@ today = dt.date.today()
 #print(today-delta)
 #print(today > today-delta)
 ########################### Objects and map() ####################################
+class Person:
+    department = "School of Information"
+    def set_name(self, new_name):
+        self.name = new_name
+    def set_location(self, new_location):
+        self.location = new_location
+
+person = Person()
+person.set_name("Christopher Brooks")
+person.set_location("Ann Arbor, MI, USA")
+"""print("{} live in {} and works in the department {}".format(
+person.name, person.location, person.department
+))"""
+
+store1 = [10.00, 11.00, 12.34, 2.34]
+store2 = [9.00, 11.10, 12.34, 2.01]
+cheapest = map(min, store1, store2)
+#for item in cheapest:   print(item)
+##################### Lambda and List comprehensions #####################
+my_function = lambda a, b, c: a + b
+#print(my_function(1,2,3))
+"""my_list = []
+for number in range(0, 1000):
+    if number % 2 == 0:
+        my_list.append(number)"""
+#or
+my_list = [number for number in range(0, 1000) if number % 2 == 0]
+#print(my_list)
+############################ NumPy ############################
+import numpy as np
+
+mylist = [1, 2, 3]
+x = np.array(mylist)
+y = np.array([4, 5, 6])
+m = np.array([[7, 8, 9], [10, 11, 12]])
+#print(m.shape) #dimensions from array
+n = np.arange(0, 30, 2)
+n = n.reshape(3, 5)
+o = np.linspace(0, 4, 9)
+o.resize(3, 3)
+p = np.ones([2, 3], int)
+#print(np.ones((3, 2)))
+#print(np.zeros((2, 3)))
+#print(np.eye(3))
+#print(np.diag(y))
+#print(np.array([1, 2, 3] * 3))
+#print(np.repeat([1, 2, 3], 3))
+#print(np.vstack([p, 2*p]))
+#print(np.hstack([p, 2*p]))
+#print(x + y)
+#print(x - y)
+#print(x * y)
+#print(x / y)
+#print(x ** 2)
+#print(x.dot(y))
+z = np.array([y, y**2])
+#print(len(z), z)
+#print(z.shape)
+#print(z.T, z.T.shape, z.dtype)
+z = z.astype("f")
+#print(z.dtype)
+
+a = np.array([-4, -2, 1, 3, 5])
+#print(a.sum(), a.max(), a.min(), a.mean(), a.std())
+#print(a.argmax(), a.argmin())
+
+s = np.arange(13)**2
+r = np.arange(36)
+r.resize((6, 6))
+#print(r)
+#print(r[2, 2])
+#print(r[3, 3:6])
+#print(r[:2, :-1])
+#print(r[-1, ::2])
+#r[r>30]=30
+#print(r)
+r2 = r[:3, :3]
+r2[:] = 0
+#print(r2)
+#To avoid this, use `r.copy` to create a copy that will not affect the original array
+r_copy = r.copy()
+r_copy[:] = 10
+#print(r_copy)
+#print(r)
+
+test = np.random.randint(0, 10, (4, 3))
+print(test)
+#for row in test:    print(row)
+#for i in range(len(test)):  print(test[i])
+#for i, row in enumerate(test):  print("row", i, "is", row)
+
+test2 = test**2
+print(test2)
+for i, j in zip(test, test2):   print(i, "+", j, "=", i+j)
