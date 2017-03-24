7d4b9e3f50ac11fe949af558a3b0128f8d5491ba
george <geocarvalho@outlook.com>
2017-03-24 00:06:29 -0300

    week one done

--- week1/week1.py
+++ week1/week1.py
@@ -147,4 +147,32 @@ print(test)
 
 test2 = test**2
 print(test2)
-for i, j in zip(test, test2):   print(i, "+", j, "=", i+j)
+#for i, j in zip(test, test2):   print(i, "+", j, "=", i+j)
+################## class questions ########################
+people = ['Dr. Christopher Brooks', 'Dr. Kevyn Collins-Thompson', 'Dr. VG Vinod Vydiswaran', 'Dr. Daniel Romero']
+
+def split_title_and_name(person):
+    return person.split()[0] + ' ' + person.split()[-1]
+
+#option 1
+for person in people:
+    print(split_title_and_name(person) == (lambda x: x.split()[0] + ' ' + x.split()[-1])(person))
+
+#option 2
+list(map(split_title_and_name, people)) == list(map(lambda person: person.split()[0] + ' ' + person.split()[-1], people))
+
+def times_tables():
+    lst = []
+    for i in range(10):
+        for j in range (10):
+            lst.append(i*j)
+    return lst
+
+times_tables() == [j*i for i in range(10) for j in range(10)]
+
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+digits = '0123456789'
+
+correct_answer = [a+b+c+d for a in lowercase for b in lowercase for c in digits for d in digits]
+
+correct_answer[:50] # Display first 50 ids
