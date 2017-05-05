58130e123f3b32b265f5a89cdec9fe30e3000c6b
george <geocarvalho@outlook.com>
2017-05-04 23:09:25 -0300

    answer 2 done

--- assignment3.py
+++ assignment3.py
@@ -1,58 +1,106 @@
 import pandas as pd
 import numpy as np
 
-def main():
-    #Starting with energy data
+def ans_one():
+    #Starting with energy data, exclude the header and footer
     energy = pd.read_excel("./data/Energy_Indicators.xls", skiprows=18, skip_footer=38, header=None)
+    # Exclude the two first columns
     energy.drop([0, 1], axis=1, inplace=True)
+    # Make your header
     energy_col = ['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable']
     energy.columns = energy_col
+    # Treat your missing values
     energy.replace("...", np.NaN, inplace=True)
+    # Convert "Energy Supply" to gigajoules
     energy["Energy Supply"] = energy["Energy Supply"].apply(lambda x: x*1000000)
+    # Replace the numbers in countries names
+    energy["Country"] = energy["Country"].replace(to_replace=["0","1","2","3","4","5","6","7","8","9"], value="", regex=True)
+    # Cleaning data
     energy["Country"].replace(to_replace={
-    "China2": "China",
-    "United States of America20": "United States",
-    "United Kingdom of Great Britain and Northern Ireland19": "United Kingdom",
-    "Japan10": "Japan",
-    "France6": "France",
-    "Italy9": "Italy",
-    "Spain16": "Spain",
-    "Australia1": "Australia",
-    "Republic of Korea": "South Korea",
     "United States of America": "United States",
     "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
+    "Republic of Korea": "South Korea",
     "China, Hong Kong Special Administrative Region": "Hong Kong"
     }, inplace=True)
+    # Cleaning strings with "()"
     energy["Country"] = energy["Country"][energy["Country"].notnull()].apply(
     lambda x: x.split(" (")[0] if x.endswith(")") else x)
-    #energy["Country"].apply(lambda x: print(x) if x.startswith("B") else x)
-    #print("\n\n")
-    #print("Energy\n", energy.head(5))
 
-    # Now, GDP data
+    # Now, GDP data, skip the header
     GDP = pd.read_csv("./data-bank/world_bank.csv", skiprows=4)
+    # Cleaning data
     GDP["Country Name"].replace(to_replace={
     "Korea, Rep.": "South Korea",
     "Iran, Islamic Rep.": "Iran",
     "Hong Kong SAR, China": "Hong Kong"
     }, inplace=True)
+    # Using the last 10 years of data
     GDP_use = pd.concat([GDP.ix[:, 0], GDP.ix[:, -12:-2]], axis=1)
+    # Change the column name for the merge
     GDP_use.rename(columns={"Country Name": "Country"}, inplace=True)
-    #GDP_use["Country"].apply(lambda x: print(x) if x.startswith("B") else x)
-    #print("GDP\n", GDP_use["Country"].tail(5))
 
     # Finally, ScimEn data
     ScimEn = pd.read_excel("./data/scimagojr-3.xlsx")
+    # Using just the 15 first countries
     ScimEn_use = ScimEn.ix[:14,:]
-    #print(ScimEn_use["Country"])
-    #print("ScimEn size: ", ScimEn_use.shape)
-    #print("Scimen\n", ScimEn["Country"].tail(5))
 
+    # Merging data
     df_use = ScimEn_use.merge(energy, how="inner", on="Country").merge(GDP_use, how="inner", on="Country").set_index("Country")
+    # Organize your data
     df_use = df_use[df_use["Rank"].notnull()]
     df_use[["Rank", "Documents", "Citable documents", "Citations", "Self-citations", "H index"]] = df_use[["Rank", "Documents", "Citable documents", "Citations", "Self-citations", "H index"]].astype(int)
     df_use[["Energy Supply", "Energy Supply per Capita", "% Renewable"]] = df_use[["Energy Supply", "Energy Supply per Capita", "% Renewable"]].astype(float)
-    print(df_use)
+    return df_use
+
+def ans_two():
+    #Starting with energy data, exclude the header and footer
+    energy = pd.read_excel("./data/Energy_Indicators.xls", skiprows=18, skip_footer=38, header=None)
+    # Exclude the two first columns
+    energy.drop([0, 1], axis=1, inplace=True)
+    # Make your header
+    energy_col = ['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable']
+    energy.columns = energy_col
+    # Treat your missing values
+    energy.replace("...", np.NaN, inplace=True)
+    # Convert "Energy Supply" to gigajoules
+    energy["Energy Supply"] = energy["Energy Supply"].apply(lambda x: x*1000000)
+    # Replace the numbers presente in the strings
+    energy["Country"] = energy["Country"].replace(to_replace=["0","1","2","3","4","5","6","7","8","9"], value="", regex=True)
+    # Cleaning data
+    energy["Country"].replace(to_replace={
+    "United States of America": "United States",
+    "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
+    "Republic of Korea": "South Korea",
+    "China, Hong Kong Special Administrative Region": "Hong Kong"
+    }, inplace=True)
+    # Cleaning strings with "()"
+    energy["Country"] = energy["Country"][energy["Country"].notnull()].apply(
+    lambda x: x.split(" (")[0] if x.endswith(")") else x)
+
+    # Now, GDP data, skip the header
+    GDP = pd.read_csv("./data-bank/world_bank.csv", skiprows=4)
+    # Cleaning data
+    GDP["Country Name"].replace(to_replace={
+    "Korea, Rep.": "South Korea",
+    "Iran, Islamic Rep.": "Iran",
+    "Hong Kong SAR, China": "Hong Kong"
+    }, inplace=True)
+    # Change the column name for the merge
+    GDP.rename(columns={"Country Name": "Country"}, inplace=True)
+
+    # Finally, ScimEn data
+    ScimEn = pd.read_excel("./data/scimagojr-3.xlsx")
+    # Using just the 15 first countries
+
+    # Merging data to take the intersection (inner)
+    df_i = pd.merge(pd.merge(energy, GDP, on="Country"), ScimEn, on="Country")
+    # Merging data to take the union (outer)
+    df_u = pd.merge(pd.merge(energy, GDP, on="Country", how="outer"), ScimEn, on="Country", how="outer")
+    return df_u.shape[0] - df_i.shape[0]
+
+def main():
+    data = ans_two()
+    print(data)
 
 if __name__ == "__main__":
     main()
