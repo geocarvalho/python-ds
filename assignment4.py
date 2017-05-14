01c65ee239044e8452c56443e594819614433a49
geocarvalho <geocarvalho@outlook.com>
2017-05-14 12:12:38 -0300

    first fuction

---
+++ assignment4.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+def get_list_of_university_towns():
+    '''Returns a DataFrame of towns and the states they are in from the
+    university_towns.txt list. The format of the DataFrame should be:
+    DataFrame( [ ["Michigan", "Ann Arbor"], ["Michigan", "Yipsilanti"] ],
+    columns=["State", "RegionName"]  )
+
+    The following cleaning needs to be done:
+
+    1. For "State", removing characters from "[" to the end.
+    2. For "RegionName", when applicable, removing every character from " (" to the end.
+    3. Depending on how you read the data, you may need to remove newline character '\n'. '''
+
+    # Use this dictionary to map state names to two letter acronyms
+    states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada',\
+     'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', \
+     'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', \
+     'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', \
+     'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', \
+     'WI': 'Wisconsin', 'MI': 'Michigan', 'IN': 'Indiana', 'NJ': 'New Jersey', \
+     'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico', \
+     'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', \
+     'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', \
+     'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', \
+     'LA': 'Louisiana', 'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', \
+     'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California', 'CO': 'Colorado', \
+     'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island', \
+     'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', \
+     'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'}
+
+    df = pd.read_table("/home/george/Git-projects/introduction-to-data-science-in-python/data/university_towns.txt", names=["Text"])
+    df['State'] = df.loc[df['Text'].str.contains('[edit]', regex=False), 'Text'].str.extract(r'(.*?)\[edit\]', expand=False)
+    df['RegionName'] = df.loc[df['State'].isnull(), 'Text'].str.extract(r'(.*?)\s*[\(\[]+.*[\n]*', expand=False)
+    df['State'] = df['State'].ffill()
+    df = df.dropna()
+
+    return df.head(20)
+
+def main():
+    data = get_list_of_university_towns()
+    print(data)
+
+if __name__ == "__main__":
+    main()
