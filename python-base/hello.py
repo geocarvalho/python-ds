d9352c2d7856c075720f127dc48c477847ecbd90
George Carvalho <geocarvalho91@gmail.com>
2022-08-15 22:03:41 +0400

    add more languages

--- python-base/hello.py
+++ python-base/hello.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
+"""Hello world multi linguas.
 
-# Comment
-print("Hello, world!")
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execucao:
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "George Carvalho"
+__license__ = "Unlicense"
+
+import os
+
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, world!"
+
+if current_language == "pt_BR":
+    msg = "Ola, mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, monde!"
+
+print(msg)
